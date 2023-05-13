40260aa65750bc096645998b0c7f78088dde0d55
natemackintosh <natemack99@icloud.com>
2023-05-13 11:55:37 -0500

    Finished TPAaccept and deny

--- src/main/java/me/plugin/teleportcommands/commands/TPAccept.java
+++ src/main/java/me/plugin/teleportcommands/commands/TPAccept.java
@@ -4,12 +4,16 @@ import me.plugin.teleportcommands.utils.TPA;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 public class TPAccept implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
-        TPA tpa = new TPA();
-        return false;
+        if(!(commandSender instanceof Player)) {
+            return false;
+        }
+        TPA.acceptTPA((Player) commandSender);
+        return true;
     }
 }
--- src/main/java/me/plugin/teleportcommands/commands/TPDeny.java
+++ src/main/java/me/plugin/teleportcommands/commands/TPDeny.java
@@ -1,9 +1,20 @@
 package me.plugin.teleportcommands.commands;
 
 
+import me.plugin.teleportcommands.utils.TPA;
+import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
 
 public class TPDeny implements CommandExecutor {
-
-
+    @Override
+    public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
+        if(!(commandSender instanceof Player)) {
+            return false;
+        }
+        TPA.denyTPA((Player) commandSender);
+        return true;
+    }
 }
